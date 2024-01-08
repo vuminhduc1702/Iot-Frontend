121ebdc118c55f637077a4a61d1d7cc2c6665eaf
vuminhduc1702 <vuminhduc1702@gmail.com>
2024-01-09 01:07:12 +0700

    fix base url

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
     <React.StrictMode>
-      <BrowserRouter basename="/fe">
+      <BrowserRouter>
         <App />
         <Toaster
           position="bottom-right"
