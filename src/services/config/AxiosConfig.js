fcc5d4717fc31164d2e9c9d1fbe533a02508ac4d
vuminhduc1702 <vuminhduc1702@gmail.com>
2024-01-08 21:27:09 +0700

    add .env file

--- src/services/config/AxiosConfig.js
+++ src/services/config/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://dungdt.id.vn";
-// const API_BASE_URL = "http://localhost:8080";
+// const API_BASE_URL = "https://dungdt.id.vn";
+const API_BASE_URL = "http://localhost:8080";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
